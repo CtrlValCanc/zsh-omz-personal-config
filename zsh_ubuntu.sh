aa57f06a8703b08ca6d16e15a551f1f36fc52596
Valeria Cancarini <valeria.cancar@gmail.com>
2023-08-31 19:08:37 +0200

    added error

--- zsh_ubuntu.sh
+++ zsh_ubuntu.sh
@@ -3,41 +3,70 @@
 echo "Setting up Zsh and Oh My Zsh..."
 
 # Install Zsh using pacman
-sudo apt install -S zsh curl git
+if ! sudo apt install zsh curl git; then
+    echo "Error: Zsh installation failed."
+    exit 1
+fi
+
+echo "Zsh installed."
 
 # Install Oh My Zsh using installation command from GitHub
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if ! sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; then
+    echo "Error: Oh My Zsh installation failed."
+    exit 1
+fi
 
-echo "Zsh and Oh My Zsh installed."
+echo "Oh My Zsh installed."
 
 # Change default shell to Zsh for the current user
-chsh -s $(which zsh)
+if ! chsh -s $(which zsh); then
+    echo "Error: Changing default shell to Zsh failed."
+    exit 1
+fi
 
 echo "Changed default shell to Zsh."
 
 # Clone zsh-autosuggestions plugin from GitHub repository
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+if ! git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions; then
+    echo "Error: Cloning zsh-autosuggestions plugin failed."
+    exit 1
+fi
 
 # Clone zsh-syntax-highlighting plugin from GitHub repository
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+if ! git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting; then
+    echo "Error: Cloning zsh-syntax-highlighting plugin failed."
+    exit 1
+fi
 
 echo "Cloned zsh-autosuggestions and zsh-syntax-highlighting plugins."
 
 # Modify the ~/.zshrc file to include zsh-autosuggestions and zsh-syntax-highlighting plugins
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+if ! sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc; then
+    echo "Error: Modifying ~/.zshrc failed."
+    exit 1
+fi
 
 # Update the Zsh configuration file
-source ~/.zshrc
+if ! source ~/.zshrc; then
+    echo "Error: Updating Zsh configuration failed."
+    exit 1
+fi
 
 echo "Updated Zsh configuration."
 
 # Install Nerd Font (Meslo) using yay
-yay -S ttf-meslo-nerd-font-powerlevel10k
+if ! yay -S ttf-meslo-nerd-font-powerlevel10k; then
+    echo "Error: Nerd Font installation failed."
+    exit 1
+fi
 
 echo "Installed Nerd Font."
 
 # Install Powerlevel10k theme for Zsh using git
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+if ! git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k; then
+    echo "Error: Installing Powerlevel10k theme failed."
+    exit 1
+fi
 
 echo "Installed Powerlevel10k theme."
 
@@ -53,7 +82,10 @@ if [ "$add_aliases" == "y" ]; then
             break
         fi
         echo "alias $new_alias" >> ~/.zshrc
-        source ~/.zshrc
+        if ! source ~/.zshrc; then
+            echo "Error: Adding alias failed."
+            exit 1
+        fi
         echo "Added alias: $new_alias"
     done
 fi
